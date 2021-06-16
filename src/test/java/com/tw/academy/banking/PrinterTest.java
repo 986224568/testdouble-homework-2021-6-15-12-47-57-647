f0249a15ef1b4813addc05fa1da1eb19e0ed40f5
Domingo <986224568@qq.com>
2021-06-16 22:23:49 +0800

    [Domingo] feature test should_call_printLine_in_console_when_call_print

--- src/test/java/com/tw/academy/banking/PrinterTest.java
+++ src/test/java/com/tw/academy/banking/PrinterTest.java
@@ -3,6 +3,7 @@ package com.tw.academy.banking;
 import com.tw.banking.Console;
 import com.tw.banking.Printer;
 import com.tw.banking.Transaction;
+import org.joda.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -11,15 +12,26 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.tw.banking.Clock.DATE_FORMAT;
+import static com.tw.banking.Printer.STATEMENT_HEADER;
+
 public class PrinterTest {
     @Mock
     Console console;
 
     @Test
     void should_call_printLine_in_console_when_call_print() {
+        MockitoAnnotations.openMocks(this);
         // given
-
+        Printer printer = new Printer(console);
+        List<Transaction> transactions = new ArrayList<>();
+        String date = LocalDate.now().toString(DATE_FORMAT);
+        int amount = 1;
+        Transaction transaction = new Transaction(date, amount);
+        transactions.add(transaction);
         // when
+        printer.print(transactions);
         // then
+        Mockito.verify(console, Mockito.times(1)).printLine(STATEMENT_HEADER);
     }
 }
