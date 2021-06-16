9015e568b8b372d19ae22086f07ff2d003992475
Domingo <986224568@qq.com>
2021-06-16 22:33:24 +0800

    [Domingo] feature test should_print_hahaha_when_call_printLine

--- src/test/java/com/tw/academy/banking/ConsoleTest.java
+++ src/test/java/com/tw/academy/banking/ConsoleTest.java
@@ -1,12 +1,25 @@
 package com.tw.academy.banking;
 
+import com.tw.banking.Console;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConsoleTest {
     @Test
     void should_print_hahaha_when_call_printLine() {
         // given
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        String str = "hahaha";
+        Console console = new Console();
         // when
+        console.printLine(str);
         // then
+        assertEquals(str + "\r\n", byteArrayOutputStream.toString());
     }
 }
