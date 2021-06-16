ff79fbc911755e5f5b2a3ed9e5c0c98df59d74b6
Domingo <986224568@qq.com>
2021-06-16 22:30:17 +0800

    [Domingo] add test should_print_hahaha_when_call_printLine

---
+++ src/test/java/com/tw/academy/banking/ConsoleTest.java
@@ -0,0 +1,12 @@
+package com.tw.academy.banking;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsoleTest {
+    @Test
+    void should_print_hahaha_when_call_printLine() {
+        // given
+        // when
+        // then
+    }
+}
