919cf8153648b6ca0c3f3bbeb63c551f1bd9d391
Domingo <986224568@qq.com>
2021-06-16 22:35:39 +0800

    [Domingo] add test should_call_todayAsString_when_call_addDeposit

---
+++ src/test/java/com/tw/academy/banking/TransactionRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.tw.academy.banking;
+
+import org.junit.jupiter.api.Test;
+
+public class TransactionRepositoryTest {
+    @Test
+    void should_call_todayAsString_when_call_addDeposit() {
+        // given
+        // when
+        // then
+    }
+}
