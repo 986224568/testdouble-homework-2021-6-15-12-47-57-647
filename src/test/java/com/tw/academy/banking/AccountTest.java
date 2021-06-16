7a1d38863cd0d520f22c2185dca83e737556d25f
Domingo <986224568@qq.com>
2021-06-16 21:21:54 +0800

    [Domingo] feature test should_call_addDeposit_from_repository_when_call_addDeposit_in_account

--- src/test/java/com/tw/academy/banking/AccountTest.java
+++ src/test/java/com/tw/academy/banking/AccountTest.java
@@ -5,6 +5,7 @@ import com.tw.banking.Printer;
 import com.tw.banking.TransactionRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class AccountTest {
@@ -17,8 +18,12 @@ public class AccountTest {
     void should_call_addDeposit_from_repository_when_call_addDeposit_in_account() {
         MockitoAnnotations.openMocks(this);
         // given
+        Account account = new Account(transactionRepository, printer);
+        int amount = 1;
         // when
+        account.deposit(amount);
         // then
+        Mockito.verify(transactionRepository, Mockito.times(1)).addDeposit(amount);
     }
 
 }
