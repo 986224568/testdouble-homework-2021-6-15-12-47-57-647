8886dfbec48ec9b412838b48d95db130c042c40e
Domingo <986224568@qq.com>
2021-06-16 21:05:17 +0800

    [Domingo] add test should_return_minus_1_when_transactionA_earlier_than_transactionB

---
+++ src/test/java/com/tw/academy/banking/TransactionTest.java
@@ -0,0 +1,24 @@
+package com.tw.academy.banking;
+
+import com.tw.banking.Transaction;
+import org.joda.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static com.tw.banking.Clock.DATE_FORMAT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionTest {
+    @Test
+    void should_return_minus_1_when_transactionA_earlier_than_transactionB() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
