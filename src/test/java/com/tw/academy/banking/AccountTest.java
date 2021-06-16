8ea71062bee59caccc311e0fefe0d7b9c3a484b5
Domingo <986224568@qq.com>
2021-06-16 21:28:29 +0800

    [Domingo] feature test should_call_addWithdraw_from_repository_when_call_addWithdraw_in_account

--- src/test/java/com/tw/academy/banking/AccountTest.java
+++ src/test/java/com/tw/academy/banking/AccountTest.java
@@ -28,9 +28,14 @@ public class AccountTest {
 
     @Test
     void should_call_addWithdraw_from_repository_when_call_addWithdraw_in_account() {
+        MockitoAnnotations.openMocks(this);
         // given
+        Account account = new Account(transactionRepository, printer);
+        int amount = 1;
         // when
+        account.withdraw(amount);
         // then
+        Mockito.verify(transactionRepository, Mockito.times(1)).addWithdraw(amount);
     }
 
 }
