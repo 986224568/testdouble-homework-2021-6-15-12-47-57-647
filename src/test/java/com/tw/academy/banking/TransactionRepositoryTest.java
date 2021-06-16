e5757b6819c1c0fb5a2b6f6c6039c55d13c9cb2f
Domingo <986224568@qq.com>
2021-06-16 22:38:15 +0800

    [Domingo] feature test should_call_todayAsString_when_call_addDeposit

--- src/test/java/com/tw/academy/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/academy/banking/TransactionRepositoryTest.java
@@ -1,12 +1,24 @@
 package com.tw.academy.banking;
 
+import com.tw.banking.Clock;
+import com.tw.banking.TransactionRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class TransactionRepositoryTest {
+    @Mock
+    Clock clock;
     @Test
     void should_call_todayAsString_when_call_addDeposit() {
+        MockitoAnnotations.openMocks(this);
         // given
+        TransactionRepository transactionRepository = new TransactionRepository(clock);
+        int mount = 1;
         // when
+        transactionRepository.addDeposit(mount);
         // then
+        Mockito.verify(clock, Mockito.times(1)).todayAsString();
     }
 }
