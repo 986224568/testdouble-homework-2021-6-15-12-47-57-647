cb5fb9350e946c41fd3cd4ae0d1a18b9041b56e3
Domingo <986224568@qq.com>
2021-06-16 21:51:21 +0800

    [Domingo] add test should_call_printLine_in_console_when_call_print

--- src/test/java/com/tw/academy/banking/PrinterTest.java
+++ src/test/java/com/tw/academy/banking/PrinterTest.java
@@ -1,17 +1,22 @@
 package com.tw.academy.banking;
 
 import com.tw.banking.Console;
+import com.tw.banking.Printer;
+import com.tw.banking.Transaction;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrinterTest {
     @Mock
     Console console;
 
     @Test
-    void should_call_printStatementLines_when_call_print() {
-        MockitoAnnotations.openMocks(this);
+    void should_call_printLine_in_console_when_call_print() {
         // given
 
         // when
